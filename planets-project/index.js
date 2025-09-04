c83a0bc1a6814a05de88655bf6d2cf4866267827
Rohan <rohanam2000@gmail.com>
2025-09-04 16:38:05 +0530

    Planets-project folder update

---
+++ planets-project/index.js
@@ -0,0 +1,41 @@
+const { error } = require('console');
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet){
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
+
+fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))
+    .on('data', (data) => {
+        // console.log('')
+        if(isHabitablePlanet(data)){
+            habitablePlanets.push(data);
+        }
+    })
+
+    .on('error', (err) => {
+        console.log(err);
+    })
+
+    .on('end', () => {
+        console.log(habitablePlanets.map((planet) => {
+            return planet['kepler_name'];
+            // return planet['kepoi_name'];
+        }));
+        console.log(habitablePlanets.map((planet) => {
+            return planet['kepoi_name'];
+        }))
+        // console.log(habitablePlanets);
+        console.log(`${habitablePlanets.length} is the amount of habitable planets from the kelper.csv`);
+        console.log('Done processing !!');
+    });
+// parse();
